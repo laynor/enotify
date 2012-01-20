7bc75d8599133f1d062c32f03dfd209503c910a4
Alessandro Piras <laynor@gmail.com>
2012-01-20 18:17:36 +0100

    Added a shell script for interacting with enotify from the command line

---
+++ esend.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+strlen () {
+    str="`echo $1`"
+    echo ${#str}
+}
+
+mkmsg () {
+    echo "|`strlen "$1"`|$1"
+}
+
+print_usage () {
+    echo $0: $1
+    echo USAGE: $0 '[[host] port] message'
+    echo You can also try
+    echo ' $' $0 [[host] port] register
+    echo and then
+    echo ' $' $0 [[host] port] example
+    to see an example.
+}
+
+check_arguments () {
+    [[ $port =~ [0-9]+ ]]
+}
+
+
+which nc &> /dev/null
+if [ ! $? -eq 0 ]
+then
+    echo "This utility needs the nc (netcat) command."
+    exit 1
+fi
+
+
+case $# in
+    1)
+	host='localhost'
+	port=5000
+	msg=$1
+	;;
+    2)
+	host='localhost'
+	port=$1
+	msg=$2
+	;;
+    3)
+	host=$1
+	port=$2
+	msg=$3
+	;;
+    *)
+	print_usage "wrong number of arguments $#"
+	exit 1
+	;;
+esac
+
+example_registration='(:register "esend.sh"
+ :handler-fn (lambda (id data)
+               (let ((buf (get-buffer-create "*esend.sh example*")))
+                  (save-current-buffer
+                    (set-buffer buf)
+                    (erase-buffer)
+                    (insert data)))))'
+                    
+example_msg='(:id "esend.sh"
+ :notification (:text "esend.sh"
+                :face :success
+                :mouse-1 (lambda nil (interactive) (switch-to-buffer "*esend.sh example*"))
+                :help "Example tooltip")
+ :data "Some example data")
+'
+    
+check_arguments
+case $msg in
+    "register")
+	msg=$example_registration
+	;;
+    "example")
+	msg=$example_msg
+	;;
+esac
+if [ $? -eq 0 ]
+then
+    m=`mkmsg "$msg"`
+    echo Sending "'$m'" to $host:$port
+    echo $m | nc $host $port
+else
+    print_usage "wrong argument format: port = $port"
+fi
+	
+
+# mkmsg $1 | nc 
