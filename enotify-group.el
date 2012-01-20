2c64a045ae9b81a46defecb79a834f94f8d37eb6
Alessandro Piras <laynor@gmail.com>
2012-01-20 19:48:00 +0100

    Added enotify-debug flag for debugging messages
    Added a defvar to silence the compiler too.

--- enotify-group.el
+++ enotify-group.el
@@ -2,3 +2,9 @@
   "Display notifications on emacs' mode line."
   :group 'modeline)
 (eval-and-compile (provide 'enotify-group))
+
+(defvar enotify-minor-mode) ;; Silencing compiler
+
+(defcustom enotify-debug nil
+  "Enable debug messages."
+  :group 'enotify)
